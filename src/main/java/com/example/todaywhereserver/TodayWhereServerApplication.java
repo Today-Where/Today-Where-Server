71119b02b3c6b67e5c8f0e772509b88d9dc73318
KMS06 <jjm336949@dsm.hs.kr>
2024-03-19 11:55:08 +0900

    setting: project setting

---
+++ src/main/java/com/example/todaywhereserver/TodayWhereServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaywhereserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayWhereServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayWhereServerApplication.class, args);
+	}
+
+}
