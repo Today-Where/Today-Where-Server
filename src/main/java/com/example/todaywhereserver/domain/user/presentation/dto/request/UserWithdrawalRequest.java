77da425a7a2a1103069abfeeb40d0810d99e2339
csh0927 <sung060927@gmail.com>
2024-04-08 13:59:40 +0900

    feat: 회원탈퇴 구현

---
+++ src/main/java/com/example/todaywhereserver/domain/user/presentation/dto/request/UserWithdrawalRequest.java
@@ -0,0 +1,10 @@
+package com.example.todaywhereserver.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserWithdrawalRequest {
+    private String password;
+}
