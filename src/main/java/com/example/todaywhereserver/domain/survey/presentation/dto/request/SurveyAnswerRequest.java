9091ddfa1c47a0d4d6a6c5f280f00c75927c64f8
KMS06 <jjm336949@dsm.hs.kr>
2024-05-24 10:26:01 +0900

    refactor: 코드 정리

--- src/main/java/com/example/todaywhereserver/domain/survey/presentation/dto/request/SurveyAnswerRequest.java
+++ src/main/java/com/example/todaywhereserver/domain/survey/presentation/dto/request/SurveyAnswerRequest.java
@@ -3,8 +3,6 @@ package com.example.todaywhereserver.domain.survey.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @NoArgsConstructor
 public class SurveyAnswerRequest {
